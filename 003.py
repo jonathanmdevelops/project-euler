e4dbf25fcb724d15c97f428fbcc2d1b8447b28df
Saint Thomas <saint-thomas-moore@users.noreply.github.com>
2015-11-13 20:07:17 +0000

    Improvements

--- 003.py
+++ 003.py
@@ -1,12 +1,5 @@
 from math import sqrt
-
-def is_prime(n):
-	start = int(sqrt(n))
-	while start > 1:
-		if n % start == 0:
-			return False
-		start -= 1
-	return True
+from prime import is_prime
 
 
 n = 600851475143
